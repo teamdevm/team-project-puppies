39d369cb5e5612942347e463e3b6873b8d43fd62
Сергей Писцов <pistsovsergey@yandex.ru>
2022-05-29 18:55:31 +0500

    Добавлен элемент ControlEntry
    Переопределение элемента Entry для изменения стиля поля ввода текста:
    - Цвет выделенного текста
    - Цвет курсора
    - Курсор выделения
    - Линия под текстом прозрачная

---
+++ src/frontend/YoungDevelopers/YoungDevelopers/YoungDevelopers/YoungDevelopers/ControlEntry/ControlEntry.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+
+namespace YoungDevelopers
+{
+    public class ControlEntry : Entry
+    {
+        #region Bindable Properties
+        public static readonly BindableProperty MyHighlightColorProperty = BindableProperty.Create(nameof(MyHighlightColor), typeof(Color), typeof(ControlEntry));
+
+        public static readonly BindableProperty MyHandleColorProperty = BindableProperty.Create(nameof(MyHandleColor), typeof(Color), typeof(ControlEntry));
+
+        public static readonly BindableProperty MyTintColorProperty = BindableProperty.Create(nameof(MyTintColor), typeof(Color), typeof(ControlEntry));
+        #endregion
+
+        #region Properties
+        public Color MyHighlightColor
+        {
+            get => (Color)GetValue(MyHighlightColorProperty);
+            set => SetValue(MyHighlightColorProperty, value);
+        }
+        public Color MyHandleColor
+        {
+            get => (Color)GetValue(MyHandleColorProperty);
+            set => SetValue(MyHandleColorProperty, value);
+        }
+        public Color MyTintColor
+        {
+            get => (Color)GetValue(MyTintColorProperty);
+            set => SetValue(MyTintColorProperty, value);
+        }
+        #endregion
+    }
+}
