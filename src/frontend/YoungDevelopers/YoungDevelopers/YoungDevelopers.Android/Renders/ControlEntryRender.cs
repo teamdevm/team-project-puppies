7d5013335f8e7957e8003c75eec00e50904ec14c
Vasilii Balchikov <40277170+gitbleidd@users.noreply.github.com>
2022-06-07 19:49:43 +0500

    Xamarin auth form (#3)
    * Добавлена форма авторизации
    
    * Добавлен элемент ControlEntry
    
    Переопределение элемента Entry для изменения стиля поля ввода текста:
    - Цвет выделенного текста
    - Цвет курсора
    - Курсор выделения
    - Линия под текстом прозрачная
    
    * Обновление авторизации
    
    - Добавлена навигация по страницам
    - Изменен фон
    - Выравнивание текста
    - Выравнивание элементов
    - Обновлены шрифты
    - Добавлены новые цвета
    - Добавлено закругление элементов
    
    Co-authored-by: Сергей Писцов <pistsovsergey@yandex.ru>

---
+++ src/frontend/YoungDevelopers/YoungDevelopers/YoungDevelopers.Android/Renders/ControlEntryRender.cs
@@ -0,0 +1,51 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using YoungDevelopers;
+using YoungDevelopers.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using System;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Text;
+using Android.Graphics.Drawables;
+
+[assembly: ExportRenderer(typeof(ControlEntry), typeof(ControlEntryRender))]
+namespace YoungDevelopers.Droid
+{
+    class ControlEntryRender : EntryRenderer
+    {
+        public ControlEntryRender(Context context) : base(context)
+        { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (e.NewElement is ControlEntry NewControlEntry)
+            {
+                // Tint Color
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                {
+                    Control.BackgroundTintList = ColorStateList.ValueOf(NewControlEntry.MyTintColor.ToAndroid());
+                }
+                else
+                {
+                    Control.Background.SetColorFilter(new PorterDuffColorFilter(NewControlEntry.MyTintColor.ToAndroid(), PorterDuff.Mode.SrcAtop));
+                }
+
+
+                // Highlight Color
+                Control.SetHighlightColor(color: NewControlEntry.MyHighlightColor.ToAndroid());
+                Control.Background = null;
+                Control.SetBackgroundColor(color: Android.Graphics.Color.Transparent);
+            }
+
+        }
+
+    }
+}
---
+++ src/frontend/YoungDevelopers/YoungDevelopers/YoungDevelopers/RegistrationPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace YoungDevelopers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            
+            // Список полей
+
+            // CheckBox согласие на обработку
+
+            // Кнопка создать аккаунт
+        }
+    }
+}
