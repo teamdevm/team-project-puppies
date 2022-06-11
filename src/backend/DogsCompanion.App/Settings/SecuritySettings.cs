b2f0875393b46906ba42339b21fbde95b56998f7
gitbleidd <adonis7952@hotmail.com>
2022-06-12 03:02:20 +0500

    Добавлена генерация токенов

---
+++ src/backend/DogsCompanion.App/Settings/SecuritySettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogsCompanion.App.Settings
+{
+    public class SecuritySettings
+    {
+        public string JwtSecret { get; set; } = string.Empty;
+        public string JwtIssuer { get; set; } = string.Empty;
+        public string JwtAudience { get; set; } = string.Empty;
+        public int JwtExpireDays { get; set; }
+        public int RefreshTokenExpireDays { get; set; }
+    }
+}
