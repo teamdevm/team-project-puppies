2c04c4f563d9dde3edf037820cdb3034df36f418
gitbleidd <adonis7952@hotmail.com>
2022-06-26 16:52:35 +0500

    Добавлено сохранение авторизации пользователя

--- src/frontend/YoungDevelopers/YoungDevelopers/YoungDevelopers/Pages/App/App.xaml.cs
+++ src/frontend/YoungDevelopers/YoungDevelopers/YoungDevelopers/Pages/App/App.xaml.cs
@@ -12,11 +12,11 @@ namespace YoungDevelopers
 
         public App()
         {
-            CreateClient();
-            DataControl.LoadData();
+            //CreateClient();
+            //DataControl.LoadData();
             InitializeComponent();
-            DependencyService.Register<MockDataStore>();
-            MainPage = new NavigationPage(new LoginPage());
+            //DependencyService.Register<MockDataStore>();
+            //MainPage = new NavigationPage(new LoginPage());
         }
 
         private void CreateClient()
@@ -54,9 +54,70 @@ namespace YoungDevelopers
 
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+            var httpClient = new HttpClient();
+            App.Current.Properties["httpClient"] = httpClient;
 
+            IToken tokenController = new TokenController();
+            App.Current.Properties["tokenController"] = new TokenController();
+
+            string baseUrl = "https://exp-webapi.herokuapp.com/";
+            var dogsCompanionClient = new DogsCompanionClient(baseUrl, httpClient, tokenController);
+            dogsCompanionClient.UpdatedToken += UpdateTokens; // Событие по получению новых токенов
+            dogsCompanionClient.FailedServerUpdateToken += FailedServerUpdateToken;
+
+            App.Current.Properties["dogsCompanionClient"] = dogsCompanionClient;
+
+            DataControl.LoadData();
+
+            string accessToken = null;
+            try
+            {
+                accessToken = await tokenController.GetAccessTokenAsync();
+            }
+            catch (System.Exception) { }
+
+            // Если есть токен, то делаем действия аналогично авторизации
+            if (!string.IsNullOrEmpty(accessToken))
+            {
+                // Установление ключа в httpclient
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                try
+                {
+                    var userInfo = await dogsCompanionClient.GetUserInfoAsync();
+                    var dogInfo = await dogsCompanionClient.GetDogsAsync();
+
+                    var authResponse = new AuthResponse()
+                    {
+                        Id = userInfo.Id,
+                        Email = userInfo.Email,
+                        PhoneNumber = userInfo.PhoneNumber,
+                        FirstName = userInfo.FirstName,
+                        LastName = userInfo.LastName,
+                        MiddleName = userInfo.MiddleName,
+                        BirthDate = userInfo.BirthDate,
+                    };
+                    DataControl.SetAuthData(authResponse, dogInfo);
+                    DataControl.SetUserInfoItem(userInfo);
+
+
+                    MainPage = new NavigationPage(new LoginPage());
+                    App.Current.MainPage = new MainPage();
+                }
+                catch (System.Exception)
+                {
+                    
+                    App.Current.MainPage = new Page();
+                    await Application.Current.MainPage.DisplayAlert("", "Сервис не доступен", "OK");
+                    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+                }
+            }
+            else
+            {
+                MainPage = new NavigationPage(new LoginPage());
+            }
         }
 
         protected override void OnSleep()
